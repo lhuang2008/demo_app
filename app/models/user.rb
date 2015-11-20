aa7d8dbbec4263f8ea086471f987e0387adda3da
Leijun Huang <leijun.huang@gmail.com>
2015-11-20 07:12:48 -0400

    Added password to the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
+# Need the digest library for hash calculation.
+require 'digest'
+
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  # Create a virtual attribute "password"
+  # A virtual attribute is an attribute not corresponding to a column in DB.
+  attr_accessor :password
+  attr_accessible :email, :name, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -9,4 +15,33 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :format => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+
+  # Add validations to the "password" attribute.
+  # Also automatically create the virtual attribute "password_confirmation".
+  validates :password, :presence => true,
+                       :confirmation => true, # create "password_confirmation"
+                       :length => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+    return nil
+  end
+
+  private
+    
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
