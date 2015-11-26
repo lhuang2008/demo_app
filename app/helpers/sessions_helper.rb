828c16980d973de1b888e28e6a6ddfb0c30fbce6
Leijun Huang <leijun.huang@gmail.com>
2015-11-26 11:01:45 -0400

    Added sessions and cookies.

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,22 @@
+module SessionsHelper
+
+  def sign_in(user)
+    cookies.signed[:remember_token] = user.id
+    # Use the following line for permanent cookie.
+    # cookies.permanent.signed[:remember_token] = user.id
+    @current_user = user
+  end
+
+  def current_user
+    if @current_user.nil?
+      User.find_by_id(cookies.signed[:remember_token])
+    else
+      @current_user
+    end
+  end
+
+  def sign_out
+    cookies.delete(:remember_token)
+    @current_user = nil
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 DemoApp::Application.routes.draw do
+  resources :sessions, :only => [:new, :create, :destroy]
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
   match '/signup', :to => 'users#new'
-#  get "pages/home"
-#  get "pages/contact"
-#  get "pages/about"
-#  get "pages/help"
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
